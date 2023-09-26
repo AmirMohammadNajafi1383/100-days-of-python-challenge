42572f0482b68a8bd4cbd0f685e5c0ef306f9e18
Amirmohammad Najafi <amirmohammadnajafi@Amirmohammads-MacBook-Pro.local>
2023-09-26 10:43:27 +0330

    Day3

---
+++ Day13/Exercise-3.py
@@ -0,0 +1,23 @@
+# for number in range(1,101):
+#     if number % 3 == 0 or number % 5 == 0:
+#         print('FizzBuzz')
+#     if number % 3==0:
+#         print('Fizz')
+
+#     if  number % 5==0:
+#         print('Buzz')
+#     else:
+#         print([number])
+
+# Debugging
+
+for number in range(1,101):
+    if number % 3 == 0 or number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3==0:
+        print('Fizz')
+
+    elif number % 5==0:
+        print('Buzz')
+    else:
+        print(number)
